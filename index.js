9a56de9babcadbba99d7ab47b33d878042492122
laralorranesilva5-source <laralorranesilva5@gmail.com>
2026-04-16 10:37:56 -0300

    finalizando

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function mudar_conteudo_elemento() {
 
   document.getElementById("h1").innerHTML = "Hello JavaScript!";
 }
+
+text-decoration-line: underline;
+text-decoration-style: solid;
+text-decoration-color: blue;
+text-decoration-thickness: 0.2em;
